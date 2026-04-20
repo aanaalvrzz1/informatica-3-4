7ff1da1f34be1b89bd0a138a14d0883e9ac5179a
aanaalvrzz1 <annesophia060909@gmail.com>
2026-04-20 19:23:24 +0000

    recipe work done

--- recipe.c
+++ recipe.c
@@ -4,5 +4,14 @@ int main(void)
 {
 printf("Coconut Cranberry Desert \n");
 printf("\n");
+printf("1 1/2\tcups graham cracker crumbs\n");
+printf("1/2\tcup butter or margarine, melted\n");
+printf("1 can\tsweetened condensed milk\n");
+printf("1 cup\tflaked coconut\n");
+printf("1 cup\tdried cranberries\n");
+printf("2 cups\tvanilla chips\n");
+printf("1 cup\tsliced almonds or pecans\n");
+printf("\n");
+printf("1. Combine graham cracker crumbs and butter. Press into a greased 13″ x 9″ (33 x 23-cm) pan.\n");
+printf("2. Combine remaining ingredients and spread over graham cracker mixture. Bake at 350°F (175°C) for 25 minutes or until the edges are golden brown. Let cool and cut into squares.\n");
 }
-
