d14c5b57cc4bf870facc5aadb5df6986511772d8
aanaalvrzz1 <annesophia060909@gmail.com>
2026-04-21 14:55:25 +0000

    variables 1st work

---
+++ naming-variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+int number; //I fixed this by deleting the exclamation mark.
+int test_value; //i fixed this by adding a guion bajo.
+int example1; //i fixed this by changing the number 1 to the end.
+float int_count; //i fixed this by adding "_count".
+printf("Variables created successfully.\n");
+}
