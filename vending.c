28504dfb34f018ae60b45692cc8762071fda3915
aanaalvrzz1 <annesophia060909@gmail.com>
2026-04-21 21:06:24 +0000

    vending work done

--- vending.c
+++ vending.c
@@ -9,7 +9,7 @@ int main(void)
  int quantity=15;
  char selection='A';
 printf("Item name:Skittles \n");
-printf("Price=%f \n", price);
+printf("Price=%.2f \n", price);
 printf("Quantity Available=%d \n", quantity);
 printf("Selection Code=%c \n", selection);
 printf("\n");
@@ -18,7 +18,17 @@ printf("\n");
  int quantity1=10;
  char selection1='B';
  printf("Item name:Mountain Dew \n");
- printf("Price=%f \n", price1);
+ printf("Price=%.2f \n", price1);
  printf("Quantity Available=%d \n", quantity1);
  printf("Selection Code=%c \n", selection1);
+ printf("\n");
+ //variables for item 3
+ float price2=1.05;
+ int quantity2=20;
+ char selection2='C';
+ printf("worm Gummies \n");
+ printf("Price=%.2f \n", price2);
+ printf("Quantity Available=%d \n", quantity2);
+ printf("Selection Code=%c \n", selection2);
+ printf("\n");
 }
