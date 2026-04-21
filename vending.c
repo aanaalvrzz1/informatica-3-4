3190ba76c2a7278e07851057038243ccffd12350
aanaalvrzz1 <annesophia060909@gmail.com>
2026-04-21 15:50:03 +0000

    vending progress

---
+++ vending.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+ printf("vending machine stock\n");
+ printf("--------------------- \n");
+ //variables for item 1
+ float price=0.95;
+ int quantity=15;
+ char selection='A';
+printf("Item name:Skittles \n");
+printf("Price=%f \n", price);
+printf("Quantity Available=%d \n", quantity);
+printf("Selection Code=%c \n", selection);
+printf("\n");
+ //variables for item 2
+ float price1=1.25;
+ int quantity1=10;
+ char selection1='B';
+ printf("Item name:Mountain Dew \n");
+ printf("Price=%f \n", price1);
+ printf("Quantity Available=%d \n", quantity1);
+ printf("Selection Code=%c \n", selection1);
+}
