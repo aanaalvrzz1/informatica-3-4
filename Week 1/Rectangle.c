008f417018f3a93ff1fa0e0c4718313a781a9957
aanaalvrzz1 <annesophia060909@gmail.com>
2026-04-24 16:40:08 +0000

    Week 1 Done

---
+++ Week 1/Rectangle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+int Height;
+printf("Height of your rectangle: \n");
+scanf("%d",&Height);
+int Width;
+printf("Width of your rectangle: \n");
+scanf("%d",&Width);
+printf("your area is = %d * %d \n",Height, Width);
+}
