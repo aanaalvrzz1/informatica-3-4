df4d122640d358e5c8778850b4637d151fe8e93f
aanaalvrzz1 <annesophia060909@gmail.com>
2026-04-28 22:59:15 +0000

    rectangle work done

--- Week 1/Rectangle.c
+++ Week 1/Rectangle.c
@@ -5,11 +5,13 @@ int main(void)
 {
 int lenght;
 int width;
-printf("Enter the lenght of the rectangle"\n);
+printf("Enter the lenght of the rectangle \n");
 scanf("%d",&lenght);
 printf("Enter the width of the rectangle: \n");
 scanf("%d", &width);
 int Area =lenght*width;
-int Perimeter =(lenght+width)*2
+int Perimeter =(lenght+width)*2;
+printf("Area = %d \n", Area);
+printf("Perimeter = %d \n",Perimeter);
 }
 
