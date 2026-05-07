cf9ef223b07387be19b89823d4ef776baadaeffd
aanaalvrzz1 <annesophia060909@gmail.com>
2026-05-07 14:59:26 +0000

    minutes fixed

--- minutes.c
+++ minutes.c
@@ -2,7 +2,7 @@
 int main(void)
 {
 int Minutes;
-printf("Enter the movie run time in minutes;\n");
+printf("Enter the movie run time in minutes:");
 scanf("%d", &Minutes);
 
 int start_h;
@@ -16,7 +16,13 @@ int min = Minutes % 60;
 int end_h=start_h+hours;
 int end_m=start_m+min;
 
+if(end_m >=60) {
+    end_h = end_h + (end_m / 60); //update variable value
+    end_m=end_m % 60;// we used the modulous for getting the rest of the divison for the minutes 
+}
+
 printf("The movie will last %d hr and %dmin. \n", hours, min);
-printf("End time: %d:%d \n", end_h, end_m);
+printf("End time: %d:%02d \n", end_h, end_m); //problem 1 fixed
+
 
 }
